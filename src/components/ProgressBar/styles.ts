3e58db4799a5b6083d68dd6ffe130bce77553e0c
Bruno Alves <hello@brunoalves.app>
2022-08-24 21:14:01 +0100

    comment removed

--- src/components/ProgressBar/styles.ts
+++ src/components/ProgressBar/styles.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  /* border: 1px solid ${props => props.theme.colors.primary}; */
   width: 100%;
   height: 1.25rem;
 
